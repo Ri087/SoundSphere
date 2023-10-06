1898a36cc927ff969bf27fea44b1911e6a70dba5
AlexNbl27 <alexandre.noblet@ynov.com>
2023-10-06 11:15:33 +0200

    Search bar added

--- lib/screens/search_music.dart
+++ lib/screens/search_music.dart
@@ -15,124 +15,45 @@ class _SearchMusic extends State<SearchMusic> {
   @override
   void initState() {
     super.initState();
-    /* if (FirebaseAuth.instance.currentUser == null) {
-      WidgetsBinding.instance.addPostFrameCallback((_) =>
-          Navigator.push(context, MaterialPageRoute(
-              builder: (context) => const LoginEmail())));
-    } */
   }
 
   void leavePage() {
     Navigator.pop(context);
   }
 
+  bool typing = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF02203A),
       appBar: AppBar(
-        elevation: 0,
-        backgroundColor: const Color(0xFF02203A),
-        leading: Image.asset("assets/images/image_transparent.png"),
-        title: const Text("SoundSphere", style: TextStyle(color: Colors.white, fontFamily: 'ZenDots', fontSize: 18),),
-        actions: [
-          IconButton(
-            onPressed: () {
-              index++;
-            }, icon: const Icon(Icons.person_rounded, color: Color(0xffffffff),),)
-        ],
-      ),
-
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 15),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 45,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search a room',
-                    filled: true,
-                    fillColor: const Color(0xFF02203A),
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(7.0), borderSide: const BorderSide(width: 2.0, color: Color(0xFFFFE681))),
-                    enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(7.0), borderSide: const BorderSide(width: 2.0, color: Color(0xFFFFE681))),
-                    focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(7.0), borderSide: const BorderSide(width: 2.0, color: Color(0xFFFFE681))),
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 15.0),
-                    suffixIcon: Material(
-                      borderRadius: const BorderRadius.only(topRight: Radius.circular(5)),
-                      color: Colors.transparent,
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.search, color: Color(0xFFFFE681),size: 20)
-                      ),
-                    ),
-                  ),
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: RefreshIndicator(
-                triggerMode: RefreshIndicatorTriggerMode.anywhere,
-                backgroundColor: const Color(0xFF0EE6F1),
-                color: Colors.white,
-                onRefresh: () async {
-                },
-                child: FutureBuilder(
-                    future: publicRoomWidgetList,
-                    builder: (context, snapshot) {
-                      List<Widget> children;
-                      if (snapshot.hasData) {
-                        children = snapshot.data!;
-                      } else if (snapshot.hasError) {
-                        children = [Text("Result : ${snapshot.error}")];
-                      } else {
-                        children = [
-                          const SizedBox(
-                            height: 500,
-                            child: Center(
-                              child: SizedBox(
-                                width: 75,
-                                height: 75,
-                                child: CircularProgressIndicator(
-                                    color: Color(0xFF0EE6F1)),
-                              ),
-                            ),
-                          ),
-                        ];
-                      }
-                      return SizedBox(
-                        height: MediaQuery.of(context).size.height - 200,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ListView.builder(
-                            itemCount: children.length,
-                            itemBuilder: (ctxt /*context*/, ind) {
-                              return children[ind];
-                            },
-                          ),
-                        ),
-                      );
-                    }
-                ),
-              ),
-            )
-          ],
+        backgroundColor: const Color(0xFFFF86C9), //const Color(0xFF0EE6F1),
+        title: typing ? const TextBox() : TextButton(onPressed: (){setState(() {typing = !typing;});}, child: const Text("Click here to search a music", style: TextStyle(color: Colors.white, fontSize: 16)),),
+        leading: BackButton(
+          onPressed: () {
+            leavePage();
+          },
         ),
       ),
+    );
+  }
+}
 
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color(0xFFFF86C9),
-        foregroundColor: const Color(0xFF02203A),
-        onPressed: () {
-          index++;
-        },
-        child: const Icon(Icons.add, size: 30,),
+class TextBox extends StatelessWidget {
+  const TextBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      style: const TextStyle(color: Colors.white, fontSize: 16),
+      decoration: InputDecoration(
+        hintText: 'Search a music',
+        filled: true,
+        fillColor: const Color(0xFF02203A),
+        hintStyle: const TextStyle(color: Colors.white),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(7.0)),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 15.0),
       ),
     );
   }
